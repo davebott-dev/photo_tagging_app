1e72614e91d6bf7e00e11047236e23aecf5c12a4
= <bottenbergdave@gmail.com>
2024-12-03 09:06:42 -0500

    creating popup for char selection

--- Client/src/Index.jsx
+++ Client/src/Index.jsx
@@ -1,12 +1,22 @@
+import {useState} from 'react';
 import "./App.css";
 
 function Index() {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    open ? setOpen(false): setOpen(true);
+  }
+
   return (
     <div>
       <img
         src="https://where-is-waldo-kc.netlify.app/assets/waldo-1-CnK-GoU-.webp"
         alt="a very crowded beach"
+        onClick= {handleClick}
       ></img>
+      {open && <div>
+        Hello World
+      </div>}
     </div>
   );
 }
--- Client/src/routes/Root.jsx
+++ Client/src/routes/Root.jsx
@@ -1,27 +1,39 @@
-import {Outlet} from 'react-router-dom';
-import {useState} from 'react';
-import WatchLaterIcon from '@mui/icons-material/WatchLater';
-import {IconButton} from '@mui/material';
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import { IconButton } from "@mui/material";
 
 function Root() {
-    const [open,setOpen] =useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleOpen = () => {
-        open ? setOpen(false) : setOpen(true);
-    }
+  const handleOpen = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
 
-    return (
-        <div>
-            <div id="header">
-                <div></div>
-                <h1>Where's Waldo</h1>
-                {open && <div>timer</div>}
-                <IconButton color="primary" onClick={handleOpen}><WatchLaterIcon/></IconButton>
-                
-            </div>
-            <Outlet/>
-        </div>
-    )
+  return (
+    <div>
+      <div id="header">
+        <div></div>
+        <h1>Where's Waldo</h1>
+        {open ? (
+          <div className="timer">
+              <div>timer</div>
+              <IconButton color="primary" onClick={handleOpen}>
+              <WatchLaterIcon />
+                      </IconButton>
+          </div>
+        ) : (
+          <div className = "timer">
+              <div className="hidden">timer</div>
+              <IconButton color="primary" onClick={handleOpen}>
+              <WatchLaterIcon />
+                      </IconButton>
+          </div>
+        )}
+      </div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default Root;
